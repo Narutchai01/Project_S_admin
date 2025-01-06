95786d6d08254c594dfd2b4f9cef8b7b73fe277c
nkmbxy <nakamon2304@gmail.com>
2025-01-06 13:32:28 +0700

    update table skincare page

--- src/app/dashboard/skincare/page.tsx
+++ src/app/dashboard/skincare/page.tsx
@@ -8,42 +8,31 @@ import {
   TableBody,
   TableRow,
   TableCell,
-  Pagination,
 } from "@nextui-org/react";
 import Link from "next/link";
 
 const Skincarepage = () => {
-  const [page, setPage] = React.useState(1);
-  const rowsPerPage = 10;
-
-  const data = Array.from({ length: 40 }, (_, index) => ({
+  const data = Array.from({ length: 60 }, (_, index) => ({
     id: `00${index + 1}`,
     name: `Skincare Name ${index + 1}`,
   }));
 
-  const totalPages = Math.ceil(data.length / rowsPerPage);
-
-  const paginatedData = React.useMemo(() => {
-    const start = (page - 1) * rowsPerPage;
-    const end = start + rowsPerPage;
-    return data.slice(start, end);
-  }, [page, data]);
-
   return (
-    <div className="p-4">
-      <div className="w-full max-w-5xl mx-auto rounded-2xl border border-gray-200 shadow-md ">
+    <div className="p-6">
+      <div className="w-full max-w-5xl mx-auto rounded-2xl border border-gray-200 shadow-md">
         <Table
           aria-label="Skincare Table"
-          className={"rounded-2xl bg-red-200"}
+          removeWrapper
+          className="rounded-2xl"
         >
           <TableHeader>
-            <TableColumn>ID</TableColumn>
-            <TableColumn>Name</TableColumn>
-            <TableColumn>&nbsp;</TableColumn>
+            <TableColumn className="text-lg font-bold">ID</TableColumn>
+            <TableColumn className="text-lg font-bold">Name</TableColumn>
+            <TableColumn className="text-lg font-bold">&nbsp;</TableColumn>
           </TableHeader>
-          <TableBody items={paginatedData}>
+          <TableBody items={data}>
             {(item) => (
-              <TableRow key={item.id}>
+              <TableRow key={item.id} className="border-b border-gray-200">
                 <TableCell>
                   <Link
                     href={`/dashboard/skincare/${item.id}`}
@@ -52,6 +41,7 @@ const Skincarepage = () => {
                     {item.id}
                   </Link>
                 </TableCell>
+
                 <TableCell>
                   <Link
                     href={`/dashboard/skincare/${item.id}`}
@@ -60,6 +50,7 @@ const Skincarepage = () => {
                     {item.name}
                   </Link>
                 </TableCell>
+
                 <TableCell className="text-right">
                   <button className="text-red-500 border border-red-500 px-4 py-2 rounded-xl hover:bg-red-50">
                     Delete
@@ -69,24 +60,6 @@ const Skincarepage = () => {
             )}
           </TableBody>
         </Table>
-      </div>
-
-      <div className="flex justify-center mt-4">
-        <Pagination
-          total={totalPages}
-          page={page}
-          onChange={(page) => setPage(page)}
-          color="primary"
-          isCompact
-          showControls
-          variant="flat"
-          classNames={{
-            item: "rounded-full text-black bg-white",
-            cursor: "rounded-full bg-blue-500 text-white",
-            next: "text-black-400 hover:text-white",
-            prev: "text-black-400 hover:text-white",
-          }}
-        />
       </div>
     </div>
   );
