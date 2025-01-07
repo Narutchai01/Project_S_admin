6b3e20312c72e80e4ed9f1691e2f14035d8f729d
nkmbxy <111549955+nkmbxy@users.noreply.github.com>
2025-01-07 23:45:39 +0700

    :lipstick: update colors and font family and font size

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,21 +1,43 @@
-import {nextui} from '@nextui-org/theme';
-import type { Config } from "tailwindcss";
+import { nextui } from '@nextui-org/theme';
+import type { Config } from 'tailwindcss';
 
 export default {
-  darkMode: ["class"],
+  darkMode: ['class'],
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
       colors: {
-        "primary": "#FF6F61",
+        Bittersweet: '#FF6F61',
+        Quartz: '#4A4A4A',
+        Snow: '#FCFAFD',
+        Black: '#000000',
+        White: '#FFFFFF',
+      },
+      fontFamily: {
+        Lexend: ['Lexend'],
+      },
+      fontSize: {
+        'text-heading': ['2.1875rem', { fontWeight: '600' }], /* 35px */
+        'text-titleCrad': ['1.25rem',{ fontWeight: '400' }], /* 20px */
+        'text-contentCrad': ['1.125rem',{ fontWeight: '400' }], /* 20px */
+        'text-sideBar': ['1.125rem',{ fontWeight: '600' }], /* 18px */
+        'text-content': ['1rem',{ fontWeight: '400' }], /* 16px */
+      },
+      fontWeight: {
+        normal: '400',
+        medium: '500',
+        semibold: '600',
       },
     },
   },
-  plugins: [// eslint-disable-next-line @typescript-eslint/no-require-imports
-    require("tailwindcss-jun-layout"),nextui()],
+  plugins: [
+    // eslint-disable-next-line @typescript-eslint/no-require-imports
+    require('tailwindcss-jun-layout'),
+    nextui(),
+  ],
 } satisfies Config;
