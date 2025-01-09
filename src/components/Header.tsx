e052a5b6fe40feb3252a2f52fb5e132a9380c0ff
Narutchai <koonfluk2015@gmail.com>
2025-01-09 23:57:07 +0700

    :lipstick: create header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,39 @@
 "use client";
-import React from "react";
-import { usePathname } from "next/navigation";
+import React, { FC } from "react";
+import { Input, Button } from "@nextui-org/react";
+import { Search } from "lucide-react";
 
-const Header = () => {
-  const pathname = usePathname().split("/");
+interface HeaderProps {
+  category: string;
+  itemName: string;
+}
 
+const Header: FC<HeaderProps> = (props) => {
+  const { category, itemName } = props;
   return (
     <>
-      <div className=" container mx-auto px-14">{pathname.length}</div>
+      <div className="grid grid-cols-3 gap-4 p-4 w-full items-center">
+        <h1 className=" text-heading text-Quartz">
+          {itemName ? itemName : category}
+        </h1>
+        <div className="w-full">
+          <Input
+            labelPlacement="outside"
+            placeholder="Search"
+            radius="full"
+            startContent={<Search />}
+            type="text"
+          />
+        </div>
+        <div className="flex justify-center">
+          <Button
+            className="bg-Bittersweet text-white w-1/3 items-center"
+            radius="full"
+          >
+            Add {category}
+          </Button>
+        </div>
+      </div>
     </>
   );
 };
