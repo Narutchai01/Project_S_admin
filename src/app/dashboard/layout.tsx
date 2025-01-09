432bb8dce2196b4a8490b507b6bbfa0f8c27a7e8
Narutchai <koonfluk2015@gmail.com>
2025-01-09 23:18:42 +0700

    :lipstick: create sidebar

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -2,17 +2,25 @@
 
 import Header from "@/components/Header";
 import SideBar from "@/components/SideBar";
-import React from "react";
+import React , { useState ,ReactNode}  from "react";
+import { usePathname } from "next/navigation";
+
 
 const DashBoardlayout = ({
   children,
-}: Readonly<{ children: React.ReactNode }>) => {
+}: Readonly<{ children: ReactNode }>) => {
+
+  const pathname = usePathname().split("/");
+  const category = pathname[2];
+  const [itemName, setItemName] = useState<string>("");
+  
+
   return (
     <div className="jun-layout">
-      <header className="jun-header bg-red-600"><Header/></header>
+      <header className="jun-header bg-red-600"><Header category={category}/></header>
       <main className="jun-content bg-white-600">{children}</main>
       <aside className="jun-edgeSidebar">
-        <div className="jun-edgeContent"><SideBar/></div>
+        <div className="jun-edgeContent"><SideBar category={category}/></div>
       </aside>
       <footer className="jun-footer">Footer</footer>
     </div>
