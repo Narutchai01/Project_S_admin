64eeb5ec416ce5730249c6835428b354dbc927eb
nkmbxy <111549955+nkmbxy@users.noreply.github.com>
2025-01-15 01:31:46 +0700

    💄 update skinType page

---
+++ src/app/dashboard/skin/add/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import React from "react";
+import FormAddSkin from "@/components/skin/formAddSkin";
+
+function AddSkinpage() {
+  return (
+    <div>
+      <FormAddSkin />
+    </div>
+  );
+}
+
+export default AddSkinpage;
--- src/serverAction/skin.tsx
+++ src/serverAction/skin.tsx
@@ -1,24 +1,62 @@
 import { fetchInstance } from "@/lib/fecthInstance";
-
+import { CheckCookie, getToken } from "@/serverAction/server_action";
 
 export const fetchSkin = async () => {
-    const response = await fetchInstance("/skin", {
-      method: "GET",
-    }).catch((error) => {
-      console.error("Error:", error);
-    });
-    return response;
-  };
+  const response = await fetchInstance("/skin", {
+    method: "GET",
+  }).catch((error) => {
+    console.error("Error:", error);
+  });
+  return response;
+};
 
 export const deleteSkin = async (id: number) => {
-    try {
-        const response = await fetchInstance(`/admin/skin/${id}`, {
-        method: "DELETE",
-        });
-        console.log("Success:", response);
-        return response;
-    } catch (error) {
-        console.error("Error:", error);
-        return null;
-    }
-    }
+  try {
+    const response = await fetchInstance(`/admin/skin/${id}`, {
+      method: "DELETE",
+    });
+    console.log("Success:", response);
+    return response;
+  } catch (error) {
+    console.error("Error:", error);
+    return null;
+  }
+};
+
+export const addSkin = async (formData: FormData) => {
+  await CheckCookie();
+  const token = await getToken();
+  formData.append("image", formData.get("file") as Blob);
+  const response = await fetchInstance("/admin/skin", {
+    method: "POST",
+    headers: {
+      token: token,
+    },
+    body: formData,
+  }).catch((error) => {
+    console.error("Error:", error);
+    return error;
+  });
+  return response;
+}
+
+export const fetchSkinById = async (id: string) => {
+  const response = await fetchInstance(`/skin/${id}`, {
+    method: "GET",
+  }).catch((error) => {
+    console.error("Error:", error);
+  });
+  return response;
+}
+
+export const updateSkin = async (id: number, formData: FormData) => {
+  formData.append("image", formData.get("file") as Blob);
+  const response = await fetchInstance(`/admin/skin/${id}`, {
+    method: "PUT",
+    body: formData,
+  }).catch((error) => {
+    console.error("Error:", error);
+    return error;
+  });
+  return response;
+}
