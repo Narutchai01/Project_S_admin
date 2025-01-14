5ce8986a34122f2e88476fecaccb0407442949a3
nkmbxy <111549955+nkmbxy@users.noreply.github.com>
2025-01-14 21:56:44 +0700

    :wrench: update nextConfig

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ["s.isanook.com","redsjeqoqvsmuloaqzeo.supabase.co"], 
+    domains: ["s.isanook.com","redsjeqoqvsmuloaqzeo.supabase.co","s.isanook.com","qgxunldaghfxbofuvrdx.supabase.co"], 
   },
 };
 
