6f2dc3e40d439d4b0291f9c0df97ddea73cbab8f
Narutchai <koonfluk2015@gmail.com>
2025-01-23 19:19:15 +0700

    🧑‍💻 add func search in skicarepage

--- src/app/dashboard/skincare/page.tsx
+++ src/app/dashboard/skincare/page.tsx
@@ -4,23 +4,38 @@ import React, { useState, useEffect } from "react";
 import { Iskincare } from "@/interface/skincare";
 import { fectchSkincares } from "@/serverAction/skincare";
 import SkincareTable from "@/components/skincare/skincareTable";
+import { useContext } from "react";
+import { DashBoardContext } from "@/app/dashboard/layout";
 
 const Skincarepage = () => {
   const [skincares, setSkincares] = useState<Iskincare[]>([]);
+  const [skincareFilter, setSkincareFilter] = useState<Iskincare[]>([]);
+  const context = useContext(DashBoardContext);
+
+  const { search } = context!;
 
   useEffect(() => {
     const fetchSkincareData = async () => {
       const response = await fectchSkincares();
       setSkincares(response.data);
     };
+
+    const filterSkincare = (search: string) => {
+      const filter = skincares.filter((skincare) =>
+        skincare.name.toLowerCase().includes(search.toLowerCase())
+      );
+      setSkincareFilter(filter);
+    };
+
     fetchSkincareData();
-  }, []);
+    filterSkincare(search);
+  }, [search, skincares]);
 
   return (
     <div>
-      <SkincareTable skincares={skincares} />
+      <SkincareTable skincares={skincareFilter} />
     </div>
   );
-}
+};
 
 export default Skincarepage;
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,27 +1,32 @@
 "use client";
-import React, { FC } from "react";
-import { Input, Button } from "@nextui-org/react";
+import React, { FC} from "react";
+import { Button, Input } from "@nextui-org/react";
 import { Search } from "lucide-react";
 import Link from "next/link";
 
 interface HeaderProps {
   category: string;
+  setSearch?: (search: string) => void;
 }
 
 const Header: FC<HeaderProps> = (props) => {
-  const { category } = props;
+  const { category ,setSearch } = props;
+
   return (
     <>
       <div className=" flex flex-row w-full justify-between items-center container mx-auto px-6">
         <h1 className=" text-heading text-Quartz">{category}</h1>
         <div className=" w-full container mx-auto px-6">
-          <Input
-            labelPlacement="outside"
-            placeholder="Search"
-            radius="full"
-            startContent={<Search />}
-            type="text"
-          />
+          {category === "skincare" && (
+            <Input
+              labelPlacement="outside"
+              placeholder="Search"
+              radius="full"
+              startContent={<Search />}
+              type="text"
+              onChange={ (e) => setSearch && setSearch(e.target.value)}
+            />
+          )}
         </div>
         <div className="">
           <Link id={`add-${category}`} href={`/dashboard/${category}/add`}>
